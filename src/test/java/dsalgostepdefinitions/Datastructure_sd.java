1bf02c996f94646dfad56a3d87a4cbf4a3658e2b
SrividhyaKumar <vidhya821@gmail.com>
2024-05-02 15:51:58 -0400

    Create Datastructure_sd.java
    Add Stepdefinition for Data structure

---
+++ src/test/java/dsalgostepdefinitions/Datastructure_sd.java
@@ -0,0 +1,81 @@
+package dsalgostepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Datastructure_sd {
+
+	@Given(": The user is on DS Algo Homepage")
+	public void the_user_is_on_ds_algo_homepage() {
+	    
+	   
+	}
+
+	@When(": The user clicks on Signin button")
+	public void the_user_clicks_on_signin_button() {
+	    
+	   
+	}
+
+	@When(": The user enters {string} as username and {string} as password and clicks on login button")
+	public void the_user_enters_as_username_and_as_password_and_clicks_on_login_button(String string, String string2) {
+	    
+	   
+	}
+
+	@Then(":The user should get message as {string} in homepage")
+	public void the_user_should_get_message_as_in_homepage(String string) {
+	    
+	   
+	}
+
+	@When(": The user clicks on Get Started button of Data structures")
+	public void the_user_clicks_on_get_started_button_of_data_structures() {
+	    
+	   
+	}
+
+	@Then(": The user should be navigated to {string} page")
+	public void the_user_should_be_navigated_to_page(String string) {
+	    
+	   
+	}
+
+	@Given(": The user is on Data Structure Introduction page")
+	public void the_user_is_on_data_structure_introduction_page() {
+	    
+	   
+	}
+
+	@When(": The user clicks on Time Complexity")
+	public void the_user_clicks_on_time_complexity() {
+	    
+	   
+	}
+
+	@Then(": The user should be redirected to {string} of Data structures-Introduction")
+	public void the_user_should_be_redirected_to_of_data_structures_introduction(String string) {
+	    
+	   
+	}
+
+	@When(": The user clicks on {string} button.")
+	public void the_user_clicks_on_button(String string) {
+	    
+	   
+	}
+
+	@Then(": The user should be redirected to a page having an tryEditor with a Run button to test")
+	public void the_user_should_be_redirected_to_a_page_having_an_try_editor_with_a_run_button_to_test() {
+	    
+	   
+	}
+
+	@Then(": The user enter the python code and click Run")
+	public void the_user_enter_the_python_code_and_click_run() {
+	    
+	   
+	}
+
+}
