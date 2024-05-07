9272b8e62f16597ff9fa1750e878337498a86735
SrividhyaKumar <vidhya821@gmail.com>
2024-05-06 21:11:56 -0400

    Updating the tree page
    Updating the tree page

--- src/test/java/dsalgopages/Treepage_pf.java
+++ src/test/java/dsalgopages/Treepage_pf.java
@@ -1,14 +1,174 @@
 package dsalgopages;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class Treepage_pf {
+import dsalgoutilities.PythonExcelReader;
 
+public class Treepage_pf {
+  
+	   @FindBy (linkText = "Sign in") WebElement signin;
+	   @FindBy (xpath = "//input[@name='username']") WebElement username;
+	  @FindBy (xpath = "//input[@name='password']") WebElement password;
+	   @FindBy (xpath = "//input[@value='Login']") WebElement login;
+	 @FindBy (xpath = "//div[@role='alert']")WebElement message;
+	   @FindBy (xpath = "//a[@href='tree']")WebElement getStarted ;
+	   @FindBy (xpath = "//a[@href='overview-of-trees']")WebElement overview ;
+	   @FindBy (xpath = "//a[@href='terminologies']")WebElement terminologies ;
+	   @FindBy (xpath = "//a[@href='types-of-trees']")WebElement typesoftree ;
+	   @FindBy (xpath = "//a[@href='tree-traversals']")WebElement traversals ;
+	   @FindBy (xpath = "//a[@href='traversals-illustration']")WebElement illustration ;
+	   @FindBy (xpath = "//a[@href='binary-trees']")WebElement binarytree ;
+	   @FindBy (xpath = "//a[@href='types-of-binary-trees']")WebElement typebinary ;
+	   @FindBy (xpath = "//a[@href='implementation-in-python']")WebElement implementationpython ;
+	   @FindBy (xpath = "//a[@href='binary-tree-traversals']")WebElement btreetraversal ;
+	   @FindBy (xpath = "//a[@href='implementation-of-binary-trees']")WebElement impbtree ;
+	   @FindBy (xpath = "//a[@href='applications-of-binary-trees']")WebElement appbtree ;
+	   @FindBy (xpath = "//a[@href='binary-search-trees']")WebElement bsearch ;
+	   @FindBy (xpath = "//a[@href='implementation-of-bst']")WebElement impofbst ;
+	   @FindBy (xpath = "//a[@href='/tryEditor']")WebElement tryhere;
+	   @FindBy (xpath = "//form[@id='answer_form']/div/div/div/textarea")WebElement python_editor;
+	   @FindBy (xpath = "//button[@type='button']")WebElement run;
+	   @FindBy (xpath = "//pre[@id='output']")WebElement output;
+	   PythonExcelReader excel = new PythonExcelReader();
+					
 	 WebDriver driver;
+	 Loginpage_pf loginpage=new Loginpage_pf(driver);
 	public Treepage_pf(WebDriver driver)
 	{ 
 		PageFactory.initElements(driver, this);
 		this.driver = driver; 
+		
+		
 		}
+//	public void Homepage() { driver.get("https://dsportalapp.herokuapp.com/home"); }
+//	
+//	public void signin()
+//	{
+//		signin.click();
+//				
+//	}
+//	
+//	public void enterusernameandpassword(String username1,String password1)
+//	{
+//		username.sendKeys(username1);
+//		password.sendKeys(password1);
+//		login.click();
+//	}
+//
+//	public String getmessage()
+//	{
+//		String value=message.getText();
+//		return value;
+//	}
+
+	public void clickgetstarted() { getStarted.click(); }
+	
+	public void clickOnOverview() {
+		driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+		overview.click();
+	}
+	
+	public void tryHere() {
+		tryhere.click();
+		
+	}
+	
+	public void clickTerminologies() {
+		driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+		terminologies.click();
+			
+		}		
+		
+		public void typesOfTrees() {
+			
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			typesoftree.click();
+			
+		}
+		
+		public void traversalIllustration() {
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			traversals.click();
+		}
+		
+		public void binaryTrees() {
+			
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			binarytree.click();
+		}
+		
+		public void typesOfBinaryTrees() {
+			
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			typebinary.click();
+		}
+		
+		public void implementationInPython() {
+			
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			implementationpython.click();
+		}
+		
+		public void binaryTreeTraversal() {
+			
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			btreetraversal.click();
+		}
+		
+		public void implementationOfBinaryTrees() {
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			impbtree.click();
+		}
+		
+		public void applicationOfBinaryTrees() {
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			appbtree.click();
+		}
+		
+		
+		public void binarySearchTree() {
+			
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			bsearch.click();
+		}
+		
+		public void implementationOfBST() {
+			
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			impofbst.click();
+		}
+		
+		public void treeTraversal() {
+			driver.navigate().to("https://dsportalapp.herokuapp.com/tree/");
+			traversals.click();
+			
+		}
+		
+		public void pythonEditor() throws IOException {
+			
+			WebElement try_editor_clear=python_editor;
+			String[] list = excel.fileReader();
+			Actions actions= new Actions(driver);
+			actions.click(try_editor_clear).keyDown(Keys.CONTROL).sendKeys("\u0061").sendKeys(Keys.BACK_SPACE).keyUp(Keys.CONTROL).perform();
+			python_editor.sendKeys(list[0]);
+			run.click();
+			System.out.println("Output is :"+output.getText());
+			
+			actions.click(try_editor_clear).keyDown(Keys.CONTROL).sendKeys("\u0061").sendKeys(Keys.BACK_SPACE).keyUp(Keys.CONTROL).perform();
+			python_editor.sendKeys(list[1]);
+			run.click();
+			System.out.println("Alert is :"+driver.switchTo().alert().getText() );
+			driver.switchTo().alert().accept();		
+			}
+		
+		
+	
 }
--- src/test/java/dsalgostepdefinitions/Treepage_sd.java
+++ src/test/java/dsalgostepdefinitions/Treepage_sd.java
@@ -1,6 +1,11 @@
 package dsalgostepdefinitions;
 
+import java.io.IOException;
+
+import org.junit.Assert;
+
 import dsalgodrivers.DriverFactory;
+import dsalgopages.Datastructurepage_pf;
 import dsalgopages.Treepage_pf;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -9,112 +14,107 @@ import io.cucumber.java.en.When;
 public class Treepage_sd {
 	
   Treepage_pf treepage=new Treepage_pf(DriverFactory.getDriver());
-  
-	@Given(": The user is on DS Algo Portal homepage")
-	public void the_user_is_on_ds_algo_portal_homepage() {
-	    
-	   
-	}
+  Datastructurepage_pf datapage=new  Datastructurepage_pf(DriverFactory.getDriver());
 
+    
 	@When(": The user clicks on Get Started button of Tree")
 	public void the_user_clicks_on_get_started_button_of_tree() {
-	    
-	   
+	    treepage.clickgetstarted();
+	    System.out.println("User clicks on Get Started");
 	}
 
 	@Then(": The user should be navigated to the Tree page")
 	public void the_user_should_be_navigated_to_the_tree_page() {
-	    
+		 System.out.println("User get navigated to Tree page");
 	   
 	}
 
 	@When(": The user clicks on Overview of Trees")
 	public void the_user_clicks_on_overview_of_trees() {
 	    
-	   
+	   treepage.clickOnOverview();
 	}
 
 	@Then(": The user should be directed to the {string} of Tree Page")
 	public void the_user_should_be_directed_to_the_of_tree_page(String string) {
-	    
+		System.out.println("User navigated to "+string+" page");
 	   
 	}
 
 	@Then(": The user clicks the {string} button.")
 	public void the_user_clicks_the_button(String string) {
-	    
+	    treepage.tryHere();
 	   
 	}
 
 	@Then(": user should be redirected to a page having a TryEditor with a Run button to test")
-	public void user_should_be_redirected_to_a_page_having_a_try_editor_with_a_run_button_to_test() {
-	    
-	   
+	public void user_should_be_redirected_to_a_page_having_a_try_editor_with_a_run_button_to_test() throws IOException {
+	    treepage.pythonEditor();
 	}
 
 	@When(": The user clicks on Terminologies")
 	public void the_user_clicks_on_terminologies() {
-	    
+	    treepage.clickTerminologies();
 	   
 	}
 
 	@When(": The user clicks on Types of Trees")
 	public void the_user_clicks_on_types_of_trees() {
-	    
+	    treepage.typesOfTrees();
 	   
 	}
 
 	@When(":  The user clicks on Tree Traversals")
 	public void the_user_clicks_on_tree_traversals() {
-	    
+	    treepage.treeTraversal();
 	   
 	}
 
 	@When(":  The user clicks on Traversal Illustration")
 	public void the_user_clicks_on_traversal_illustration() {
 	    
-	   
+	   treepage.traversalIllustration();
 	}
 
 	@When(":  The user clicks on Binary Trees")
 	public void the_user_clicks_on_binary_trees() {
-	    
+	    treepage.binaryTrees();
 	   
 	}
 
 	@When(":  The user clicks on Types of Binary Trees")
 	public void the_user_clicks_on_types_of_binary_trees() {
-	    
+	    treepage.typesOfBinaryTrees();
 	   
 	}
 
 	@When(":  The user clicks on Implementation in Python")
 	public void the_user_clicks_on_implementation_in_python() {
-	    
+	    treepage.implementationInPython();
 	   
 	}
 
 	@When(":  The user clicks on Binary Tree traversals")
 	public void the_user_clicks_on_binary_tree_traversals() {
 	    
-	   
+	   treepage.binaryTreeTraversal();
 	}
 
 	@When(":  The user clicks on Implementation of Binary Trees")
 	public void the_user_clicks_on_implementation_of_binary_trees() {
-	    
+	    treepage.implementationOfBinaryTrees();
 	   
 	}
 
 	@When(":  The user clicks on Binary Search Trees")
 	public void the_user_clicks_on_binary_search_trees() {
 	    
-	   
+	   treepage.binarySearchTree();
 	}
 
 	@When(":  The user clicks on Implementation of BST")
 	public void the_user_clicks_on_implementation_of_bst() {
-	    
+	    treepage.implementationOfBST();
 	   
 	}
 
