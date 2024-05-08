58130d525b253e28197329773f84cb34b59c8adb
SrividhyaKumar <vidhya821@gmail.com>
2024-05-08 16:41:41 -0400

    Push the updated change
    Push the updated change

--- src/test/java/dsalgostepdefinitions/Treepage_sd.java
+++ src/test/java/dsalgostepdefinitions/Treepage_sd.java
@@ -43,78 +43,78 @@ public class Treepage_sd {
 
 	@Then(": The user clicks the {string} button.")
 	public void the_user_clicks_the_button(String string) {
-	    treepage.tryHere();
+	   // treepage.tryHere();
 	   
 	}
 
 	@Then(": user should be redirected to a page having a TryEditor with a Run button to test")
 	public void user_should_be_redirected_to_a_page_having_a_try_editor_with_a_run_button_to_test() throws IOException {
-	    treepage.pythonEditor();
+	 //   treepage.pythonEditor();
 	}
 
 	@When(": The user clicks on Terminologies")
 	public void the_user_clicks_on_terminologies() {
-	    treepage.clickTerminologies();
+	    //treepage.clickTerminologies();
 	   
 	}
 
 	@When(": The user clicks on Types of Trees")
 	public void the_user_clicks_on_types_of_trees() {
-	    treepage.typesOfTrees();
+	   // treepage.typesOfTrees();
 	   
 	}
 
 	@When(":  The user clicks on Tree Traversals")
 	public void the_user_clicks_on_tree_traversals() {
-	    treepage.treeTraversal();
+	   // treepage.treeTraversal();
 	   
 	}
 
 	@When(":  The user clicks on Traversal Illustration")
 	public void the_user_clicks_on_traversal_illustration() {
 	    
-	   treepage.traversalIllustration();
+	  // treepage.traversalIllustration();
 	}
 
 	@When(":  The user clicks on Binary Trees")
 	public void the_user_clicks_on_binary_trees() {
-	    treepage.binaryTrees();
+	   // treepage.binaryTrees();
 	   
 	}
 
 	@When(":  The user clicks on Types of Binary Trees")
 	public void the_user_clicks_on_types_of_binary_trees() {
-	    treepage.typesOfBinaryTrees();
+	   // treepage.typesOfBinaryTrees();
 	   
 	}
 
 	@When(":  The user clicks on Implementation in Python")
 	public void the_user_clicks_on_implementation_in_python() {
-	    treepage.implementationInPython();
+	   // treepage.implementationInPython();
 	   
 	}
 
 	@When(":  The user clicks on Binary Tree traversals")
 	public void the_user_clicks_on_binary_tree_traversals() {
 	    
-	   treepage.binaryTreeTraversal();
+	   //treepage.binaryTreeTraversal();
 	}
 
 	@When(":  The user clicks on Implementation of Binary Trees")
 	public void the_user_clicks_on_implementation_of_binary_trees() {
-	   treepage.implementationOfBinaryTrees();
+	  // treepage.implementationOfBinaryTrees();
 	   
 	}
 
 	@When(":  The user clicks on Binary Search Trees")
 	public void the_user_clicks_on_binary_search_trees() {
 	    
-	   treepage.binarySearchTree();
+	   //treepage.binarySearchTree();
 	}
 
 	@When(":  The user clicks on Implementation of BST")
 	public void the_user_clicks_on_implementation_of_bst() {
-	    treepage.implementationOfBST();
+	   // treepage.implementationOfBST();
 	   
 	}
 
